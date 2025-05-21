0b2bf499e3efdd9e9b46a3e8e7b8a818af1a8dfe
sravani-k-5 <sravanikondapallisravani@gmail.com>
2025-05-21 15:55:27 +0530

    change

--- favscript.js
+++ favscript.js
@@ -1,78 +1,148 @@
 //selecting the result container div, so that we can append favMovies inside it
+// const resultContainer = document.querySelector(".result-container");
+
+
+// let favMovies = JSON.parse(localStorage.getItem("favMovies"));
+
+
+// favMovies.forEach((id) => {
+//   getData(id); 
+// });
+
+
+// async function getData(movieID) {
+//   console.log(movieID);
+//   const result = await fetch(`http://www.omdbapi.com/?i=${movieID}&apikey=b2b1bcd6`); 
+//   const movieDetails = await result.json(); 
+
+//   AddMovie(movieDetails); 
+// }
+
+
+// const AddMovie = (details) => {
+//   const child = document.createElement("div"); 
+
+//   child.setAttribute("id", details.imdbID); 
+//   child.setAttribute("class", "result-grid"); 
+
+//   child.innerHTML = `<div class="movie-poster">
+//         <img src="${
+//           details.Poster != "N/A" ? details.Poster : "../notFound.png"
+//         }" alt="movie-poster">
+//         </div>
+
+//         <div class="movie-info">
+//             <h3 class="movie-title">${details.Title}</h3>
+//             <ul class="movie-misc-info">
+//                 <li class="year">Year: ${details.Year}</li>
+//                 <li class="rated">Ratings: ${details.Rated}</li>
+//                 <li class="released">Released: ${details.Released}</li>
+//             </ul>
+
+//             <p class="genre"><b>Genre: </b>${details.Genre}</p>
+//             <p class="writer"><b>Writer: </b> ${details.Writer}</p>
+//             <p class="actors"><b>Actors: </b> ${details.Actors}</p>
+//             <p class="plot"><b>Plot: </b> ${details.Plot}</p>
+//             <p class="language"><b>Language: </b> ${details.Language}</p>
+//             <p class="awards"><b>Awards: <i class="fa-solid fa-award"></i></b> ${details.Awards}</p>
+//         </div> 
+//         `;
+
+  
+//   const btn = document.createElement("button");
+//   btn.setAttribute("class", "delete-btn"); 
+//   btn.innerHTML = `<i data-id="${details.imdbID}" class="fa-solid fa-trash">`;
+
+//   btn.addEventListener("click", deleteMovie); 
+//   child.appendChild(btn); 
+
+//   resultContainer.appendChild(child); 
+// };
+
+
+// const deleteMovie = (e) => {
+  
+//   const delID = e.target.dataset.id;
+
+
+//   const movie = document.getElementById(`${delID}`);
+
+ 
+//   movie.remove();
+
+  
+//   favMovies = favMovies.filter((id) => id != delID);
+
+  
+//   localStorage.setItem("favMovies", JSON.stringify(favMovies));
+// };
+
+// Select the result container div
 const resultContainer = document.querySelector(".result-container");
 
-//Get data from localstorage
-let favMovies = JSON.parse(localStorage.getItem("favMovies"));
+// Get data from localStorage
+let favMovies = JSON.parse(localStorage.getItem("favMovies")) || [];
 
-//Get all Favourite movies
+// Get all Favourite movies
 favMovies.forEach((id) => {
-  getData(id); // Get Movie from API with ID
+  getData(id); // Fetch each movie
 });
 
-//Get Movies from Server
+// Get movie details from API
 async function getData(movieID) {
-  console.log(movieID);
-  const result = await fetch(`http://www.omdbapi.com/?i=${movieID}&apikey=b2b1bcd6`); //Get data from API using IMDB id
-  const movieDetails = await result.json(); //Make data readable
-
-  AddMovie(movieDetails); //Add to DOM
+  const result = await fetch(`http://www.omdbapi.com/?i=${movieID}&apikey=b2b1bcd6`);
+  const movieDetails = await result.json();
+  AddMovie(movieDetails);
 }
 
-//Add movie to DOM
+// Add movie to DOM
 const AddMovie = (details) => {
-  const child = document.createElement("div"); //Create movie box
+  const child = document.createElement("div");
+  child.setAttribute("id", details.imdbID);
+  child.setAttribute("class", "result-grid");
 
-  child.setAttribute("id", details.imdbID); //Set unique id to delete exact movie
-  child.setAttribute("class", "result-grid"); //Add CSS
+  child.innerHTML = `
+    <div class="movie-poster">
+      <img src="${details.Poster !== "N/A" ? details.Poster : "../notFound.png"}" alt="movie-poster">
+    </div>
 
-  child.innerHTML = `<div class="movie-poster">
-        <img src="${
-          details.Poster != "N/A" ? details.Poster : "../notFound.png"
-        }" alt="movie-poster">
-        </div>
+    <div class="movie-info">
+        <h3 class="movie-title">${details.Title}</h3>
+        <ul class="movie-misc-info">
+            <li class="year">Year: ${details.Year}</li>
+            <li class="rated">Ratings: ${details.Rated}</li>
+            <li class="released">Released: ${details.Released}</li>
+        </ul>
 
-        <div class="movie-info">
-            <h3 class="movie-title">${details.Title}</h3>
-            <ul class="movie-misc-info">
-                <li class="year">Year: ${details.Year}</li>
-                <li class="rated">Ratings: ${details.Rated}</li>
-                <li class="released">Released: ${details.Released}</li>
-            </ul>
+        <p class="genre"><b>Genre: </b>${details.Genre}</p>
+        <p class="writer"><b>Writer: </b>${details.Writer}</p>
+        <p class="actors"><b>Actors: </b>${details.Actors}</p>
+        <p class="plot"><b>Plot: </b>${details.Plot}</p>
+        <p class="language"><b>Language: </b>${details.Language}</p>
+        <p class="awards"><b>Awards: <i class="fa-solid fa-award"></i></b> ${details.Awards}</p>
+    </div>
+  `;
 
-            <p class="genre"><b>Genre: </b>${details.Genre}</p>
-            <p class="writer"><b>Writer: </b> ${details.Writer}</p>
-            <p class="actors"><b>Actors: </b> ${details.Actors}</p>
-            <p class="plot"><b>Plot: </b> ${details.Plot}</p>
-            <p class="language"><b>Language: </b> ${details.Language}</p>
-            <p class="awards"><b>Awards: <i class="fa-solid fa-award"></i></b> ${details.Awards}</p>
-        </div> 
-        `;
+  // ✅ Add tick mark
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "tick-remove";
+  checkbox.checked = true;
+  checkbox.dataset.id = details.imdbID;
 
-  //Create button for each favourite movie
-  const btn = document.createElement("button");
-  btn.setAttribute("class", "delete-btn"); // Add CSS
-  btn.innerHTML = `<i data-id="${details.imdbID}" class="fa-solid fa-trash">`; //Set unique id to delete exact movie
+  // ✅ If unchecked, remove movie
+  checkbox.addEventListener("change", function () {
+    if (!this.checked) {
+      const delID = this.dataset.id;
+      document.getElementById(delID)?.remove();
+      favMovies = favMovies.filter((id) => id !== delID);
+      localStorage.setItem("favMovies", JSON.stringify(favMovies));
+    }
+  });
 
-  btn.addEventListener("click", deleteMovie); // Add event listener to each button
-  child.appendChild(btn); //Add button to Movie
+  // Add tick mark to poster
+  child.querySelector(".movie-poster").appendChild(checkbox);
 
-  resultContainer.appendChild(child); //Add movie to DOM
-};
-
-//Delete movie from DOM
-const deleteMovie = (e) => {
-  //Get the id of the movie
-  const delID = e.target.dataset.id;
-
-  //Get the specific movie
-  const movie = document.getElementById(`${delID}`);
-
-  //Delete movie from view
-  movie.remove();
-
-  //Delete the movie from list
-  favMovies = favMovies.filter((id) => id != delID);
-
-  //Add new data of favMovies to localstorage
-  localStorage.setItem("favMovies", JSON.stringify(favMovies));
+  // Add to DOM
+  resultContainer.appendChild(child);
 };
